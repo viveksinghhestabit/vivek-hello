389363dffaa64d5823075431db536ee2dc566292
viveksingh.hestabit <viveksingh.hestabit@gmail.com>
2023-02-01 20:02:39 +0530

    m

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'viveksinghdeveloper/password',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'be991f5b9f9b6680a167e9e0d665621749dd8668',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'viveksinghdeveloper/password' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'be991f5b9f9b6680a167e9e0d665621749dd8668',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
